9931fbc38015a1a18f911ae8c43bf55bbaad3f8d
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-04-13 13:27:28 +0530

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,16 @@
+package com.linkcode.vehicleRentalSystem;
+
+public class Admin {
+
+	final private static String username="admin";
+	final private static String password="admin@123";
+	
+	public static boolean authenticate(String user, String pass) {
+		
+		if(username.equals(user)&&password.equals(pass)) {
+			return true;
+		}
+		
+		return false;
+	}
+}
